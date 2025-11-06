4fca069ff46bbec16c62df355bd58a1ea527da3a
duduszchaida <eduardoszchaida@gmail.com>
2025-11-06 14:04:13 -0300

    correções cors

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,9 @@ async function bootstrap() {
       'https://cibernauta-frontend.vercel.app'
     ],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    exposedHeaders: ['Authorization'],
   });
 
   app.useGlobalPipes(new ValidationPipe({
